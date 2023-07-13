dfa0a4104836af547e61c0ad279b5d9a1f646111
CharlesOuverleaux <charlesantoine.ouverleaux@gmail.com>
2023-07-13 14:20:49 +0200

    style question type

--- components/forms/ProfileForm/FormQuestionType/FormQuestionType.tsx
+++ components/forms/ProfileForm/FormQuestionType/FormQuestionType.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import React, { FC } from "react";
 
 interface FormQuestionTypeProps {
@@ -6,8 +7,22 @@ interface FormQuestionTypeProps {
 
 const FormQuestionType: FC<FormQuestionTypeProps> = ({ type }) => {
   return (
-    <div>
-      <p>{type}</p>
+    <div
+      className={clsx(
+        "rounded-lg w-24",
+        type === "required" ? "bg-tertiary-light" : "bg-secondary-light"
+      )}
+    >
+      <p
+        className={clsx(
+          "capitalize p-1",
+          type === "required"
+            ? "text-tertiary-standard"
+            : "text-secondary-standard"
+        )}
+      >
+        {type}
+      </p>
     </div>
   );
 };
